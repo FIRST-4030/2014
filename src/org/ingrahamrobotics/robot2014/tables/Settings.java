e2ea4c3a66cb13bddc214d7d0eef9e2de51b347d
Dabo Ross <daboross@daboross.net>
2014-03-08 12:24:42 -0800

    Set encoder distance again

--- src/org/ingrahamrobotics/robot2014/tables/Settings.java
+++ src/org/ingrahamrobotics/robot2014/tables/Settings.java
@@ -34,7 +34,7 @@ public class Settings implements DotNetTable.DotNetTableEvents {
         defaultSettings.setValue(CMUCAM_DEFAULT_COLOR, "RED");
         defaultSettings.setValue(AUTOCOMMAND_USE_ENCODERS, "true");
         defaultSettings.setValue(AUTOCOMMAND_STOP_TIME, "0.001");
-        defaultSettings.setValue(AUTOCOMMAND_ENCODER_DISTANCE, "25100");
+        defaultSettings.setValue(AUTOCOMMAND_ENCODER_DISTANCE, "23000");
         defaultSettings.setInterval(3000);
         driverSettings.onChange(this);
         driverSettings.onStale(this);
